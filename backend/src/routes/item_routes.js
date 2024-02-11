ec4a940f83a990d4f5a22d75e90c85757d13863d
Daniel Lescano <d.lescano.129@gmail.com>
2024-02-10 22:44:17 -0300

    rename list items

--- backend/src/routes/item_routes.js
+++ backend/src/routes/item_routes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { listsItem, getItem } from "../controllers/item_controller.js";
 const router = express.Router();
 
-router.get("/items", (req, res) => ListsItem(req, res));
+router.get("/items", (req, res) => listsItem(req, res));
 router.get("/items/:id", (req, res) => getItem(req, res));
 
 export default router;
